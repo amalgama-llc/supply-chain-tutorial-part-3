ff2541262a24bd4437dec83de4dbc8b9bd06367d
KZ <103562495+zkamsim@users.noreply.github.com>
2022-07-25 16:56:36 +0400

    Update to latest platform libs

--- bundles/com.company.tutorial3.application/src/com/company/tutorial3/application/scenario/ScenarioLoader.java
+++ bundles/com.company.tutorial3.application/src/com/company/tutorial3/application/scenario/ScenarioLoader.java
@@ -9,8 +9,8 @@ public class ScenarioLoader {
 	private ScenarioLoader() {}
 
 	public static EMFExcelLoader<Scenario> loadExcel(EMFExcelTransform<Scenario> emfExcelTransform, String filePath) {
-		EMFExcelLoader<Scenario> loader = emfExcelTransform.createLoader(filePath).setDefaultRelease(0);
-		loader.migrateAndLoad();
+		EMFExcelLoader<Scenario> loader = emfExcelTransform.createLoader(filePath);
+		loader.load();
 		return loader;
 	}
 }
